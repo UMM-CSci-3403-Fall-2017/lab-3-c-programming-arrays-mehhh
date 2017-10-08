909c6d8a8b44124bb4ea178062febfd69cdba14e
Kyle M Foss <fossx229@falcon.morris.umn.edu>
2017-10-08 01:07:28 -0500

    --This compiles and might have some errors.

--- mergesort/mergesort.c
+++ mergesort/mergesort.c
@@ -1,50 +1,53 @@
 #include <stdlib.h>
-#include <string.h>
 #include <stdbool.h>
 
 #include "mergesort.h"
 
-void mergesortRange(int* values, int startIndex, int endIndex) {
-  int rangeSize = endIndex-startIndex;
-  int midPoint=0;
-  if(rangeSize >=2) {
-    midPoint = (startIndex+endIndex) /2;
-    mergesortRange(values, startIndex, midPoint);
-    mergesortRange(values, midPoint, endIndex);
-    mergeRanges(values, startIndex, midPoint, endIndex);
+  bool needsSorting (int rangeSize) {
+    return rangeSize >= 2;
   }
-}
 
-void mergesortRanges(int* values, int startIndex, int midPoint, int endIndex) {
-  int rangeSize = endIndex -startIndex;
-  int* destination = (int*) calloc(rangeSize, sizeof(int));
-  int firstIndex = startIndex;
-  int secondIndex = midPoint;
-  int copyIndex=0;
-  int i = 0;
-  while(firstIndex < midPoint && secondIndex < endIndex) {
-    if(values[firstIndex] < values[secondIndex]) {
-      destination[copyIndex] = values[firstIndex];
-      ++firstIndex;
-    } else {
-      destination[copyIndex] = values[secondIndex];
-      ++secondIndex;
+  void mergeRanges(int* values, int startIndex, int midPoint, int endIndex) {
+    int i=0;
+    int rangeSize = endIndex- startIndex;
+    int* destination = (int*) calloc(rangeSize+1, sizeof(int));
+    int firstIndex = startIndex;
+    int secondIndex = midPoint;
+    int copyIndex = 0;
+    while(firstIndex < midPoint && secondIndex < endIndex) {
+      if(values[firstIndex] < values[secondIndex]) {
+	destination[copyIndex] = values[firstIndex];
+	++firstIndex;
+      } else {
+	destination[copyIndex] = values[secondIndex];
+	++secondIndex;
+      }
+      ++copyIndex;
     }
-    ++copyIndex;
+    while(firstIndex < midPoint) {
+      destination[copyIndex] =values[firstIndex];
+      ++copyIndex;
+      ++firstIndex;
+    }
+    while(secondIndex < endIndex) {
+      destination[copyIndex] = values[secondIndex];
+    }
+
+    for(i=0; i < rangeSize; ++i) {
+      values[i + startIndex] = destination[i];
+    }
   }
-  while (firstIndex < midPoint) {
-    destination[copyIndex] = values[firstIndex];
-    ++copyIndex;
-    ++firstIndex;
+    void mergesortRange(int* values, int startIndex, int endIndex) {
+    int rangeSize = endIndex- startIndex;
+
+    if(needsSorting(rangeSize)) {
+      int midPoint = (startIndex + endIndex) / 2;
+      mergesortRange(values, startIndex, midPoint);
+      mergesortRange(values, midPoint, endIndex);
+      mergeRanges(values, startIndex, midPoint, endIndex);
+    }
   }
 
-  while (secondIndex < endIndex) {
-    destination[copyIndex] = values[secondIndex];
-    ++copyIndex;
-    ++secondIndex;
-  }
-  for ( i = 0; i < rangeSize; ++i) {
-    values[i + startIndex] = destination[i];
-  }
-  free(destination);
+void mergesort(int a, int* values) {
+  mergesortRange(values, 0, a);
 }
