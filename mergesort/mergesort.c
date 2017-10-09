eafebfe3928e7311f446c7af2fb633a768c795fa
fossx229 <fossx229@morris.umn.edu>
2017-10-09 10:41:40 -0500

    --added documentation

--- mergesort/mergesort.c
+++ mergesort/mergesort.c
@@ -2,12 +2,19 @@
 #include <stdbool.h>
 
 #include "mergesort.h"
-
+//checks if the array is bigger than 1
   bool needsSorting (int rangeSize) {
     return rangeSize >= 2;
   }
 
   void mergeRanges(int* values, int startIndex, int midPoint, int endIndex) {
+
+    /*
+     * Assume that the two ranges are sorted:
+     *   (forall i | startIndex <= i <= j < midPoint : values[i] <= values[j])
+     *   (forall i | midPoint <= i <= j < endIndex : values[i] <= values[j])
+     * then merge them into a single sorted array, copy that back, and return.
+     */
     int i=0;
     int rangeSize = endIndex- startIndex;
     int* destination = (int*) calloc(rangeSize+1, sizeof(int));
@@ -16,11 +23,11 @@
     int copyIndex = 0;
     while(firstIndex < midPoint && secondIndex < endIndex) {
       if(values[firstIndex] < values[secondIndex]) {
-	destination[copyIndex] = values[firstIndex];
-	++firstIndex;
+    destination[copyIndex] = values[firstIndex];
+    ++firstIndex;
       } else {
-	destination[copyIndex] = values[secondIndex];
-	++secondIndex;
+    destination[copyIndex] = values[secondIndex];
+    ++secondIndex;
       }
       ++copyIndex;
     }
@@ -31,12 +38,19 @@
     }
     while(secondIndex < endIndex) {
       destination[copyIndex] = values[secondIndex];
+      ++copyIndex;
+      ++secondIndex;
     }
 
     for(i=0; i < rangeSize; ++i) {
       values[i + startIndex] = destination[i];
     }
+
+    freeing the extra memory used by mergesort since you have to partition the array
+      free(destination);
+
 	  free(destination);
+
   }
     void mergesortRange(int* values, int startIndex, int endIndex) {
     int rangeSize = endIndex- startIndex;
@@ -48,7 +62,9 @@
       mergeRanges(values, startIndex, midPoint, endIndex);
     }
   }
+// initiializing mergesort and
+void mergesort(int end, int* values) {
+mergesortRange(values, 0, end);
 
-void mergesort(int a, int* values) {
-  mergesortRange(values, 0, a);
 }
+
